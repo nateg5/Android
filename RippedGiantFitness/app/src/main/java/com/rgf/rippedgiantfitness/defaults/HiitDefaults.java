ec825db8bb3e88e2ffcfbd69e2445c9cc54ddd40
nateg5 <nateg5@gmail.com>
2016-04-24 19:14:51 -0500

    made defaults classes more consistent

--- RippedGiantFitness/app/src/main/java/com/rgf/rippedgiantfitness/defaults/HiitDefaults.java
+++ RippedGiantFitness/app/src/main/java/com/rgf/rippedgiantfitness/defaults/HiitDefaults.java
@@ -3,20 +3,47 @@ package com.rgf.rippedgiantfitness.defaults;
 import com.rgf.rippedgiantfitness.helper.LogHelper;
 import com.rgf.rippedgiantfitness.helper.SharedPreferencesHelper;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by Nate on 1/18/2016.
  */
 public class HiitDefaults {
 
     public static void create() {
-        if(SharedPreferencesHelper.getLocalPreferences().containsKey(SharedPreferencesHelper.HIIT_GO)) {
-            LogHelper.debug("HIIT Settings already exist.");
-        } else {
-            LogHelper.debug("Creating HIIT Settings defaults.");
-            SharedPreferencesHelper.getLocalPreferences().put(SharedPreferencesHelper.HIIT_GO, "");
-            SharedPreferencesHelper.getLocalPreferences().put(SharedPreferencesHelper.HIIT_REST, "");
-            SharedPreferencesHelper.getLocalPreferences().put(SharedPreferencesHelper.HIIT_ROUNDS, "");
-            SharedPreferencesHelper.commit();
+        LogHelper.debug("Creating HIIT defaults.");
+        create("", getHiitDefaults());
+        SharedPreferencesHelper.commit();
+    }
+
+    private static void create(String parent, Map<String,Object> map) {
+        if(SharedPreferencesHelper.isParentIndexValid(parent, true)) {
+            for (Map.Entry<String, Object> entry : map.entrySet()) {
+                if (entry.getValue() instanceof String) {
+                    String preferenceString = SharedPreferencesHelper.buildPreferenceString(parent, entry.getKey());
+                    if(!SharedPreferencesHelper.getLocalPreferences().containsKey(preferenceString)) {
+                        SharedPreferencesHelper.getLocalPreferences().put(preferenceString, (String) entry.getValue());
+                    }
+                } else {
+                    List<Object> list = (List<Object>) entry.getValue();
+
+                    for (int i = 0; i < list.size(); i++) {
+                        create(SharedPreferencesHelper.buildPreferenceString(parent, entry.getKey(), String.valueOf(i)), (Map<String, Object>) list.get(i));
+                    }
+                }
+            }
         }
+    }
+
+    private static Map<String,Object> getHiitDefaults() {
+        Map<String,Object> hiitDefaults = new HashMap<String,Object>() {{
+            put(SharedPreferencesHelper.HIIT_GO, "");
+            put(SharedPreferencesHelper.HIIT_REST, "");
+            put(SharedPreferencesHelper.HIIT_ROUNDS, "");
+        }};
+
+        return hiitDefaults;
     }
 }
--- RippedGiantFitness/app/src/main/java/com/rgf/rippedgiantfitness/defaults/HowToDefaults.java
+++ RippedGiantFitness/app/src/main/java/com/rgf/rippedgiantfitness/defaults/HowToDefaults.java
@@ -3,20 +3,46 @@ package com.rgf.rippedgiantfitness.defaults;
 import com.rgf.rippedgiantfitness.helper.LogHelper;
 import com.rgf.rippedgiantfitness.helper.SharedPreferencesHelper;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by Nate on 4/8/2016.
  */
 public class HowToDefaults {
 
     public static void create() {
-        if(SharedPreferencesHelper.getLocalPreferences().containsKey(SharedPreferencesHelper.HOW_TO_WORKOUTS) &&
-           SharedPreferencesHelper.getLocalPreferences().containsKey(SharedPreferencesHelper.HOW_TO_WORKOUT)) {
-            LogHelper.debug("How To properties already exists.");
-        } else {
-            LogHelper.debug("Creating How To defaults.");
-            SharedPreferencesHelper.getLocalPreferences().put(SharedPreferencesHelper.HOW_TO_WORKOUTS, "true");
-            SharedPreferencesHelper.getLocalPreferences().put(SharedPreferencesHelper.HOW_TO_WORKOUT, "true");
-            SharedPreferencesHelper.commit();
+        LogHelper.debug("Creating How To defaults.");
+        create("", getHowToDefaults());
+        SharedPreferencesHelper.commit();
+    }
+
+    private static void create(String parent, Map<String,Object> map) {
+        if(SharedPreferencesHelper.isParentIndexValid(parent, true)) {
+            for (Map.Entry<String, Object> entry : map.entrySet()) {
+                if (entry.getValue() instanceof String) {
+                    String preferenceString = SharedPreferencesHelper.buildPreferenceString(parent, entry.getKey());
+                    if(!SharedPreferencesHelper.getLocalPreferences().containsKey(preferenceString)) {
+                        SharedPreferencesHelper.setPreference(preferenceString, (String) entry.getValue(), false);
+                    }
+                } else {
+                    List<Object> list = (List<Object>) entry.getValue();
+
+                    for (int i = 0; i < list.size(); i++) {
+                        create(SharedPreferencesHelper.buildPreferenceString(parent, entry.getKey(), String.valueOf(i)), (Map<String, Object>) list.get(i));
+                    }
+                }
+            }
         }
+    }
+
+    private static Map<String,Object> getHowToDefaults() {
+        Map<String,Object> howToDefaults = new HashMap<String,Object>() {{
+            put(SharedPreferencesHelper.HOW_TO_WORKOUTS, "true");
+            put(SharedPreferencesHelper.HOW_TO_WORKOUT, "true");
+        }};
+
+        return howToDefaults;
     }
 }

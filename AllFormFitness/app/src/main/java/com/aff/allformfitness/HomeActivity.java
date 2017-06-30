7eccce73e298c74c7fffaa9d7e9930dffa0e7d1f
nateg5 <nateg5@gmail.com>
2017-06-30 14:20:14 -0500

    added splash screen for loading

--- AllFormFitness/app/src/main/java/com/aff/allformfitness/HomeActivity.java
+++ AllFormFitness/app/src/main/java/com/aff/allformfitness/HomeActivity.java
@@ -25,6 +25,7 @@ public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme_NoActionBar);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);

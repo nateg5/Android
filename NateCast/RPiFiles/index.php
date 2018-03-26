680ff5f30961432079210b288326383fdecb8a9b
nateg5 <nateg5@gmail.com>
2018-03-26 16:04:15 -0500

    initial commit of rpi3 files

---
+++ NateCast/RPiFiles/index.php
@@ -0,0 +1,35 @@
+<?php
+    $url = $_GET["url"];
+    if(strlen($url) > 0) {
+        $file = fopen("url.txt", "w");
+        fwrite($file, $url);
+        fclose($file);
+    }
+
+    $full = $_GET["full"];
+    if(strlen($full) > 0) {
+        $file = fopen("full.txt", "w");
+        fwrite($file, $full);
+        fclose($file);
+    }
+    
+    $key = $_GET["key"];
+    if(strlen($key) > 0) {
+        $file = fopen("key.txt", "w");
+        fwrite($file, $key);
+        fclose($file);
+    }
+
+    if(strlen($url) > 0) {
+        $message = "Launching $url";
+        if(strcmp($full, "true") == 0) {
+            $message .= " in full screen mode";
+        }
+        $message .= "!";
+        if(strcmp($url, "close") == 0) {
+            $message = "Closing Chrome";
+        }
+        
+        echo $message;
+    }
+?>
---
+++ NateCast/RPiFiles/index.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+while true; do
+    url=$(head -1 /var/www/html/url.txt)
+    url_length=${#url}
+    if [ $url_length -gt 0 ]
+    then
+        echo "" > /var/www/html/url.txt
+    fi
+
+    full=$(head -1 /var/www/html/full.txt)
+    full_length=${#full}
+    if [ $full_length -gt 0 ]
+    then
+        echo "" > /var/www/html/full.txt
+    fi
+    
+    key=$(head -1 /var/www/html/key.txt)
+    key_length=${#key}
+    if [ $key_length -gt 0 ]
+    then
+        echo "" > /var/www/html/key.txt
+    fi
+
+    if [ "$url" == "close" ]
+    then
+        echo "pkill chromium"
+        pkill chromium
+    elif [ $url_length -gt 0 ] 
+    then
+        chromium-browser $url & 
+        if [ "$full" == "true" ]
+        then
+            sleep 5
+            xdotool key F11 & 
+        fi
+    fi
+    
+    if [ $key_length -gt 0 ]
+    then
+        xdotool key $key &
+    fi
+    sleep 1
+done

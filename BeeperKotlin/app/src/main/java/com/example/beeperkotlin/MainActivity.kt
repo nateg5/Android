83b42184b4db6af9a5c69d11ff298b406fb758a5
Nate Gardner <ngardner1@wayfair.com>
2020-12-21 10:46:03 -0600

    - initial commit for beeper kotlin app

---
+++ BeeperKotlin/app/src/main/java/com/example/beeperkotlin/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.beeperkotlin
+
+import android.media.AudioManager
+import android.media.ToneGenerator
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlin.concurrent.timer
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val toneGenerator = ToneGenerator(AudioManager.STREAM_MUSIC, ToneGenerator.MAX_VOLUME)
+
+        timer("beep timer", false, 1000L, 1000L) {
+            toneGenerator.startTone(ToneGenerator.TONE_PROP_BEEP)
+        }
+    }
+}
